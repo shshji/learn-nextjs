e0c1a2a3bcbb3fd26951090b3588637feb42329c
laimona <shshijilai@gmail.com>
2021-10-12 22:21:13 +0900

    ♻️ create component　

---
+++ components/Footer.js
@@ -0,0 +1,14 @@
+export function Footer() {
+  return (
+      <footer>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Thanks by{' '}
+          <img src="/vercel.svg" alt="Vercel" className="logo" />
+        </a>
+      </footer>
+  );
+}
