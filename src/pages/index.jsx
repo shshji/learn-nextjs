93ebc263e1497347d4ef80d5679b6d23494645d3
laimona <shshijilai@gmail.com>
2021-10-17 16:31:09 +0900

    ✨ useState to try string&boolean

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,11 +7,25 @@ import { Main } from 'src/components/Main';
 export default function Home() {
 
   const [count, setCount] = useState(1);
+  const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState(true);
   const handleClick = useCallback(() => {
     if(count < 10){
       setCount((foo) => foo + 1);
     }
   },[count]);
+
+  const handleChange = useCallback((e) => {
+    if(e.target.value.length > 5){
+      alert("5文字以内にしてください");
+      return;
+    }
+    setText(e.target.value.trim());
+  },[]);
+  
+  const handleShow = useCallback(() => {
+    setIsShow((isShow) => !isShow);
+  },[]);
 
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue"
@@ -27,8 +41,12 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <h1>{count}</h1>
+      {isShow ? <h1>{count}</h1> : null}
       <button onClick={handleClick}>button</button>
+      <button onClick={handleShow}>
+        {isShow ? "非表示" : "表示"}
+      </button>
+      <input type="text" value={text} onChange={handleChange}/>
       <Main page="index"/>
 
       <Footer />
