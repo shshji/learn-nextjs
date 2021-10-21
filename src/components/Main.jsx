33b821f6f81bfc8e014059f5321774ebc45fff87
laimona <shshijilai@gmail.com>
2021-10-21 22:25:46 +0900

    ✨ useState Liftup between components

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,39 @@
 import { Headline } from "src/components/Headline";
 import { Links } from "src/components/Links";
+import { useCallback, useState } from "react";
+const ITEMS =[
+  { href:"https://nextjs.org/docs",
+    title:"Documentation \u2192",
+    description:"Find in-depth information about Next.js features and API." 
+  },
+  { href:"https://nextjs.org/learn",
+    title:"Learn \u2192",
+    description:"Learn about Next.js in an interactive course with quizzes!" 
+  },
+  { href:"https://github.com/vercel/next.js/tree/master/examples",
+    title:"Examples \u2192",
+    description:"Discover and deploy boilerplate example Next.js projects." 
+  },
+  { href:"https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+    title:"Deploy \u2192",
+    description:"Instantly deploy your Next.js site to a public URL with Vercel." 
+  },
+  
+  ]
 
 export function Main(props) {
+  const [items, setItems] = useState(ITEMS);
+  const handleReduce = useCallback(() => {
+    setItems(prevItems =>{
+      return prevItems.slice(0, prevItems.length - 1);
+    })
+  },[])
 	return (
 		<main>
-			<Headline page={props.page}>
-        <code>pages/{props.page}.js</code>
+			<Headline page={props.page} handleReduce={handleReduce}>
+        <code>{items.length}</code>
       </Headline>
-			<Links />
+			<Links items={items}/>
 			<style jsx>{`
         .container {
           min-height: 100vh;
